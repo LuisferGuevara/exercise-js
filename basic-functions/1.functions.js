864514b1e1bb6a6575fa184596daa79dc5294e51
Luisfer Guevara <luisferguevaraa@gmail.com>
2022-10-04 12:07:06 +0200

    Age calculator by Santiago

--- basic-functions/1.functions.js
+++ basic-functions/1.functions.js
@@ -14,3 +14,22 @@ function sum(numberOne , numberTwo) {
 console.log(sum(5,8));
 
 
+let yearOfBirth = 1997; 
+let currentYear = 2022
+
+let date = new Date();
+currentYear = date.getFullYear()
+let myAge = currentYear - yearOfBirth;
+console.log('Tu edad es', myAge);
+// -------------------------
+function calcMyAge(currentYear, yearOfBirth){
+    const myAge = currentYear - yearOfBirth
+    return myAge
+}
+let yourAge= calcMyAge(currentYear, yearOfBirth);
+console.log('Tu edad es',yourAge);
+// ---------------
+let today = new Date();
+let ageCalculator = birthYear => today.getFullYear() - birthYear
+console.log('Tu edad es', ageCalculator(1997));
+
